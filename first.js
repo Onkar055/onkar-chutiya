e8397405f9b15493c51fb6d71b815759cf61d5a0
Onkar salve <onkarlpsalve@gmail.com>
2024-07-04 18:59:02 +0530

    first cpmmit

---
+++ first.js
@@ -0,0 +1,64 @@
+console.log("tumcha coollege");
+console.log("happy birthday omi");
+console.log("good morning");
+alert("nikal yahase");
+alert("gelas kay");
+
+
+
+
+
+age=45;
+console.log(age);
+
+car="mercedies";
+console.log(car);
+
+gender="male";
+console.log(gender);
+
+model="new";
+console.log(model);
+
+doc="adhar";
+console.log(doc);
+
+x=null;
+console.log(null);
+
+price=99.99
+console.log(price);
+
+
+let fullName="onkar";
+fullName="dada";
+console.log(fullName);
+fullName="haddi";
+console.log(fullName)
+
+
+
+fullName="onkar";
+console.log(fullName);
+
+let fullSurname="salve";
+console.log(fullSurname);
+
+value=67;
+console.log(value);
+
+subject="mathmatics";
+console.log(subject);
+
+
+alert("please go from here")
+
+
+let name1="onkar";
+console.log(name1);
+
+
+relative=78;
+relative=45;
+
+const relative=56;
